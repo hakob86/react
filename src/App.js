0730211b94a183e3dc339578aa1e31a614b20fbb
hakob86 <Hakobpoghosyan1986@gmail.com>
2022-08-22 21:03:49 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
 
 const App = () => {
