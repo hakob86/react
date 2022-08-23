c09c1c6a37ea602f63398d66da34571fac47698e
hakob86 <Hakobpoghosyan1986@gmail.com>
2022-08-23 19:24:41 +0400

    gg

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
 
-const App = () => {
+const App = (props) => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-        <Dialogs />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
       </div>
-      {/* <Profile /> */}
-    </div>
+    </BrowserRouter>
   );
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import s from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <a href="/profile">Profile</a>
+        <NavLink to="/profile" className={s.activeLink}>
+          Profile
+        </NavLink>
+      </div>
+      <div className={`${s.item} ${s.active}`}>
+        <NavLink to="/dialogs" className={s.activeLink}>
+          Messages
+        </NavLink>
       </div>
       <div className={s.item}>
-        <a href="/dialogs">Messages</a>
+        <NavLink to="/news">News</NavLink>
       </div>
       <div className={s.item}>
-        <a>News</a>
+        <NavLink to="/music">Music</NavLink>
       </div>
       <div className={s.item}>
-        <a>Music</a>
-      </div>
-      <div className={s.item}>
-        <a>Settings</a>
+        <NavLink to="/settings">Settings</NavLink>
       </div>
     </nav>
   );
